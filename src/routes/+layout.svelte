67082ba3fb774535988549b4d25ec418528c2b63
Nunoki <79094039+Nunoki@users.noreply.github.com>
2023-11-12 16:04:05 +0100

    add initial layout with r6 styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<div class="wrapper">
+	<h1>Op filter</h1>
+	<slot />
+</div>
+
+<style lang="sass">
+	:global(body)
+		font-family: R6S-RegItalic,sans-serif
+		color: white
+
+		background: url(https://static-dm.ubisoft.com/siege/prod/7907369fa863844fc1ae432a9ca0e610.jpg)
+		background-size: 100%
+
+	:global(h1,h2)
+		text-transform: uppercase
+		font-weight: normal
+
+	@font-face 
+		font-family: R6S-RegItalic
+		src: url("https://static-dm.ubisoft.com/siege/prod/7c574ea662392ab39c09ed79280cf5b2.woff2") format("woff2"), url("https://static-dm.ubisoft.com/siege/prod/1a098e45a853edc663c9505d21770af9.woff") format("woff")
+		font-style: normal
+		font-weight: 400
+</style>
