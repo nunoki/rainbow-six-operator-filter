e87735bcf43b43e4182b92a3840afdeada9728f3
Nunoki <79094039+Nunoki@users.noreply.github.com>
2023-11-12 18:38:32 +0100

    remove the R6S font from global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 		padding: 1px
 		height: 100%
 
-		font-family: R6S-RegItalic,sans-serif
+		font-family: -apple-system,BlinkMacSystemFont,"Segoe UI","Noto Sans",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"
 		color: white
 
 		background: url(https://static-dm.ubisoft.com/siege/prod/7907369fa863844fc1ae432a9ca0e610.jpg)
@@ -18,6 +18,7 @@
 
 	:global(h1,h2)
 		text-transform: uppercase
+		font-family: R6S-RegItalic,sans-serif
 		font-weight: normal
 
 	.wrapper
@@ -25,7 +26,7 @@
 		margin: 0 auto
 		padding: 0 1rem
 
-	@font-face 
+	@font-face
 		font-family: R6S-RegItalic
 		src: url("https://static-dm.ubisoft.com/siege/prod/7c574ea662392ab39c09ed79280cf5b2.woff2") format("woff2"), url("https://static-dm.ubisoft.com/siege/prod/1a098e45a853edc663c9505d21770af9.woff") format("woff")
 		font-style: normal
