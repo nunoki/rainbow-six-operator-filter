2c729c6de36944d2716145a522520b07a1316204
Nunoki <79094039+Nunoki@users.noreply.github.com>
2023-11-14 02:23:46 +0100

    fix so filler elements dont get hover effects

--- src/routes/OperatorList.svelte
+++ src/routes/OperatorList.svelte
@@ -79,7 +79,7 @@
 		{/each}
 
 		{#each Array(8) as _}
-			<span class="operator" />
+			<span class="filler" />
 		{/each}
 	{:else}
 		<div class="no-results">
@@ -96,81 +96,83 @@
 		justify-content: center
 		flex-wrap: wrap
 
-		.operator
+		.operator,
+		.filler
 			position: relative
 			flex: 1 1 calc(12.5% - 5px)
 			text-decoration: none
 
-			.card
-				padding-bottom: 190%
-				position: relative
-				background-color: rgba(255,255,255,.15)
+			&.operator
+				.card
+					padding-bottom: 190%
+					position: relative
+					background-color: rgba(255,255,255,.15)
 
-				.inner
-					position: absolute
-					top: 0
-					left: 0
-					width: 100%
-					height: 100%
-					display: flex
-					flex-direction: column
+					.inner
+						position: absolute
+						top: 0
+						left: 0
+						width: 100%
+						height: 100%
+						display: flex
+						flex-direction: column
 
-					.pic
-						min-height: 0
-						max-height: 100%
-						flex: 1
+						.pic
+							min-height: 0
+							max-height: 100%
+							flex: 1
 
-					.name
-						padding: .25rem
-						text-transform: uppercase
-						text-align: center
-						font-family: R6S-RegItalic,sans-serif
-						font-size: 26px
-						color: #24262a
-						background-color: #c3c3c3
-
-			.link
-				opacity: 0
-				position: absolute
-				padding: .25rem
-				right: 10%
-				width: 80%
-				text-align: center
-				white-space: nowrap
-				text-decoration: none
-				fill: purple
-				color: purple
-				background-color: white
-
-				&.link--ubisoft
-					top: 1rem
-
-				&.link--fandom
-					top: 3.5rem
-
-				&:hover
-					color: white
-					fill: white
-					background-color: purple
-
-				&:active
-					background-color: black
-
-			.icon-note
-				position: absolute
-				z-index: 2
-				bottom: 1rem
-				right: 1rem
-				font-size: 3rem
-				font-weight: bold
-				color: red
-				cursor: help
-
-			&:hover
-				outline: 2px solid white
+						.name
+							padding: .25rem
+							text-transform: uppercase
+							text-align: center
+							font-family: R6S-RegItalic,sans-serif
+							font-size: 26px
+							color: #24262a
+							background-color: #c3c3c3
 
 				.link
-					opacity: 1
+					opacity: 0
+					position: absolute
+					padding: .25rem
+					right: 10%
+					width: 80%
+					text-align: center
+					white-space: nowrap
+					text-decoration: none
+					fill: purple
+					color: purple
+					background-color: white
+
+					&.link--ubisoft
+						top: 1rem
+
+					&.link--fandom
+						top: 3.5rem
+
+					&:hover
+						color: white
+						fill: white
+						background-color: purple
+
+					&:active
+						background-color: black
+
+				.icon-note
+					position: absolute
+					z-index: 2
+					bottom: 1rem
+					right: 1rem
+					font-size: 3rem
+					font-weight: bold
+					color: red
+					cursor: help
+
+				&:hover
+					outline: 2px solid white
+
+					.link
+						opacity: 1
 
 		.no-results
 			padding: 4rem 0
